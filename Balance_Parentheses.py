612fbae18a5cdd1bdb36bf64d8f74f4a56710e3e
Nikhil Parekh <nksp777@gmail.com>
2020-08-10 15:12:12 -0700

    Added Balance Parentheses question using stack implementation in python

---
+++ Balance_Parentheses.py
@@ -0,0 +1,31 @@
+# class Stack(object):
+#     def __init__(self):
+#         self.stack = []
+
+#     def isEmpty(self):
+#         return self.stack == []
+
+#     def push(self,element):
+#         self.stack.append(element)
+
+#     def pop(self):
+#         self.stack.pop
+
+def Check_Balance_Parentheses(s):
+    opening = ['[','{','(']
+    match = (('(',')'),('{','}'),('[',']'))
+    stack = []
+    if(len(s)%2!=0 or not s):
+        return False
+    for i in s:
+        if i in opening:
+            stack.append(i)
+        else:
+            previous = stack.pop()
+            if((previous,i) not in match):
+                return False
+    return len(stack)==0
+            
+
+
+print(Check_Balance_Parentheses('[][]{}'))
